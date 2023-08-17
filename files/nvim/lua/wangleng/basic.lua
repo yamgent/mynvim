3ad4628b6213a9f96bc75a5a60a336d03a5c92b4
Maurice Wangleng Tan <tanwangleng@outlook.com>
2023-08-17 23:06:57 +0800

    Add icons for diagnostic signs

--- files/nvim/lua/wangleng/basic.lua
+++ files/nvim/lua/wangleng/basic.lua
@@ -48,3 +48,9 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("TextYankPost", {
     command = "silent! lua vim.highlight.on_yank()"
 })
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
