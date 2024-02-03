94e30e90b98b2f6609445884d191e73e3701910b
Maurice Wangleng Tan <tanwangleng@outlook.com>
2024-02-03 13:46:23 +0800

    feat: Add lsp signature keybindings (#25)
    * feat: Add lsp signature display in insert mode
    
    * fix: Also add lsp signature for normal mode

--- lua/wangleng/plugins/lsp.lua
+++ lua/wangleng/plugins/lsp.lua
@@ -31,6 +31,9 @@ return {
                 keyset("n", "<C-j>", function() vim.diagnostic.goto_next() end, opts)
                 keyset("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
                 keyset("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+
+                keyset("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+                keyset("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
             end)
 
             -- need to be called after extend_lspconfig(), otherwise the diagnostic display doesn't work
@@ -268,3 +271,4 @@ return {
         }
     },
 }
+
