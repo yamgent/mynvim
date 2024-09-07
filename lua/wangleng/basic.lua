834815e09bcc165535bd4293516f536c9c479c58
Maurice Wangleng Tan <tanwangleng@outlook.com>
2024-09-07 09:50:17 +0800

    Add system clipboard shortcuts

--- lua/wangleng/basic.lua
+++ lua/wangleng/basic.lua
@@ -54,3 +54,9 @@ for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+-- system clipboard shortcuts
+local keyset = vim.keymap.set
+keyset("n", "<leader>c", '"+yy');
+keyset("v", "<leader>c", '"+y');
+keyset("n", "<leader>v", '"+p');
