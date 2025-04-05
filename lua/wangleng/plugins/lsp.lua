64eb5e4e7759786b328c3cf5bfd615deecbdbf90
Maurice Wangleng Tan <tanwangleng@outlook.com>
2025-04-05 19:45:32 +0800

    feat: replace inline diagnostics with diagnostic floats

--- lua/wangleng/plugins/lsp.lua
+++ lua/wangleng/plugins/lsp.lua
@@ -31,8 +31,8 @@ return {
                 callback = function(args)
                     local opts = { buffer = args.buf, remap = false }
                     local keyset = vim.keymap.set
-                    keyset("n", "<C-k>", function() vim.diagnostic.jump({ count = -1 }) end, opts)
-                    keyset("n", "<C-j>", function() vim.diagnostic.jump({ count = 1 }) end, opts)
+                    keyset("n", "<C-k>", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
+                    keyset("n", "<C-j>", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
 
                     keyset("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
                     keyset("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
@@ -53,10 +53,6 @@ return {
             vim.diagnostic.config({
                 -- for all lines, just show the error at the back
                 virtual_text = true,
-                virtual_lines = {
-                    -- only show virtual line diagnostics for the current cursor line
-                    current_line = true,
-                },
                 signs = true,
                 update_in_insert = false,
                 underline = true,
